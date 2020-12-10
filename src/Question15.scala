04893dc27028fea1671e7f00f1295846e8c6cdcc
breno alves <breno550@hotmail.com>
2020-12-10 15:40:04 -0300

    correcao

--- src/Question15.scala
+++ src/Question15.scala
@@ -5,24 +5,11 @@ object Question15 extends App {
   * Resolver usando recursão e pattern matching.
   * */
 
-  def appendinorder(lis: List[Int], n: Int): List[Int] = {
-    lis match {
-      case Nil => List(n)
-      case head::Nil if head >= n => List(n, head)
-      case head::Nil if head < n => List(head, n)
-      case head::tail if (head < n && tail.head >= n) => List(head, n):::tail
-      case head::tail => head::appendinorder(tail, n)
-    }
-  }
-
-  def solve(l1: List[Int], l2: List[Int]): List[Int] = {
-    l1 match {
-      case Nil => l2
-      case head::Nil => solve(Nil, appendinorder(l2, head))
-      case head::tail => solve(tail, appendinorder(l2, head))
-    }
+  def solve(a: List[Int]): List[Int] = a match {
+    case Nil => Nil
+    case xs => xs.min :: solve(xs.diff(List(xs.min)))
   }
 
   val lis = List(1,3,2,3,5,10,8)
-  println(solve(lis, List()))
+  println(solve(lis))
 }
