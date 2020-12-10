32af4a70fc9950f05809e16e692d37417bb08c1b
breno alves <breno550@hotmail.com>
2020-12-10 10:52:51 -0300

    question3

--- src/Question3.scala
+++ src/Question3.scala
@@ -1,8 +1,17 @@
-class Question3 extends App {
+object Question3 extends App{
   /*
   Implemente uma função que recebe um array e troca os valores de posições
   adjacentes. Por exemplo: Array(1, 2, 3, 4, 5, 6) resulta em Array(2, 1, 4, 3, 6, 5).
   Resolver usando recursão e pattern matching.
   */
 
+  def solve(lis: List[Int], acc: List[Int]): List[Int] = lis match {
+    case Nil => acc
+    case lis if lis.length >= 2 => solve(lis.tail.tail, lis.head::lis.tail.head::acc)
+    case lis if lis.length < 2 => solve(lis.tail, lis.head::acc)
+  }
+
+  val lis = List[Int](1, 2, 3, 4, 5, 6)
+
+  println(solve(lis.reverse, List()))
 }
---
+++ src/Question9.scala
@@ -0,0 +1,6 @@
+object Question9 extends App {
+  /*
+  * Implemente uma função que recebe 2 inteiros e calcula o MDC entre eles. Resolver
+  usando recursão de cauda e pattern matching.
+  * */
+}
