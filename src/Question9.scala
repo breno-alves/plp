7731918a92a96c81c0fbc460648d486e72d974f1
Breno Alves <39974954+breno-alves@users.noreply.github.com>
2020-12-10 18:18:46 -0300

    Update Question9.scala

--- src/Question9.scala
+++ src/Question9.scala
@@ -3,4 +3,9 @@ object Question9 extends App {
   * Implemente uma função que recebe 2 inteiros e calcula o MDC entre eles. Resolver
   usando recursão de cauda e pattern matching.
   * */
+  def mdc(a: Int,b: Int): Int = {
+     if(b ==0) a else mdc(b, a%b)
+  }
+
+  println(mdc(25,15))
 }
